4b7c88002decd3de53527dafb61c25f510d77e4f
JohannaHaer <johanna.haerdter@gmail.com>
2024-01-30 13:39:16 +0100

    erste fetches

--- src/context/mainProvider.jsx
+++ src/context/mainProvider.jsx
@@ -3,30 +3,52 @@ import axios from 'axios'
 export const mainContext = createContext() 
 
 const MainProvider = ({children}) => {
-    const [pokemon, setPokemon] = useState([])
+    const [pokemons, setPokemons] = useState([])
+    const [types, setTypes] = useState([])
+    const [urlPokemons, setUrlPokemons] = useState([])
 
     useEffect(() => {
         const apiFetch = async() => {
             const resp = await axios.get("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1302")
-            console.log(resp.data.results);
-            setPokemon(resp.data.results)
-            
+            setPokemons(resp.data.results)
+            console.log("Pokemons", pokemons);
+        }
+        apiFetch()
+    }, [])
+
+    // 
+
+    pokemons.map((pokemon) => {
+        return(
+            urlPokemons.push(pokemon.url)
+        )
+        
+    })
+
+    console.log(urlPokemons);
+
+
+    useEffect(() => {
+        const apiFetch = async() => {
+            const resp = await axios.get(`${urlPokemonsArray}`)
+            console.log(resp);
         }
         apiFetch()
     }, [])
 
 
-    // useEffect(() => {
-    //     const apiFetch = async() => {
-    //         const resp = await axios.get(`${}`)
-    //         console.log(resp);
-    //     }
-    //     apiFetch()
-    // }, [])
+    useEffect(() => {
+        const apiFetch = async() => {
+            const resp = await axios.get("https://pokeapi.co/api/v2/type/")
+            setTypes(resp.data.results)
+            console.log(resp.data.results);
+        }
+        apiFetch()
+    }, [])
 
     return (
         <>
-            <mainContext.Provider value={{pokemon, setPokemon}}>
+            <mainContext.Provider value={{pokemons, setPokemons, types, setTypes, urlPokemons, setUrlPokemons}}>
                 {children}
             </mainContext.Provider>
         </>
--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import axios from 'axios'
+import { mainContext } from '../../context/mainProvider'
 
 const Home = () => {
+    const {pokemons, setPokemons, urlPokemons, setUrlPokemons} = useContext(mainContext)
+
+    pokemons.map((pokemon) => {
+        return(
+            setUrlPokemons(pokemon.url)
+        )
+    })
+
+    console.log(pokemons);
+        // useEffect(() => {
+    //     const apiFetch = async() => {
+    //         const resp = await axios.get(`${}`)
+    //         console.log(resp);
+    //     }
+    //     apiFetch()
+    // }, [])
+
     return (
         <>
             
