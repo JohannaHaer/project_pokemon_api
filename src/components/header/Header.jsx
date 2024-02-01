8c0c9cbb5e10d8f60cf0824dea6ed992f733bfef
Bwalyaa <bwalya@hotmail.de>
2024-02-01 09:55:24 +0100

    darkmode

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,10 +3,13 @@ import logo from '../../assets/img/logo.png'
 import { Link } from 'react-router-dom'
 import { mainContext } from '../../context/mainProvider'
 
+
+
 const Header = () => {
     const {pokemons, setPokemons, pokemonsBackup, setPokemonsBackup} = useContext(mainContext)
     const [searchInput, setSearchInput] = useState("")
-
+    const {darkMode, toggleDarkMode} = useContext(mainContext)
+   
     const search = (event) => {
         const input = event.target.value
         setSearchInput(input)
@@ -16,7 +19,10 @@ const Header = () => {
 
     return (
         <>
-            <header>
+            <header className={darkMode ? 'dark' : 'light'}>
+                <button onClick={toggleDarkMode}>
+                    {darkMode ? 'light' : 'dark'}
+                </button>
                 <Link to='/'><img src={logo} alt="Pokémon Logo" /></Link>
                 <Link to='/menu'>Menu</Link>
                 <input type='text' placeholder='Search Pokémon' onChange={search} value={searchInput}/>
